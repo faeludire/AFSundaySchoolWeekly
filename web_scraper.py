4f3326d15b4de6a28ea9390142285dba41518014
Fiyinfolu Eludire <faeludire@gmail.com>
2018-10-03 14:33:50 +0100

    Add url

--- web_scraper.py
+++ web_scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+url = 'http://apostolicfaithweca.org/'
 
 def lesson_content_retrieval(website_url):
     page = requests.get(website_url)
