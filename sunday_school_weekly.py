e996785f435acb294f3cec8b2819e496a11c419f
Fiyinfolu Eludire <faeludire@gmail.com>
2020-06-19 19:06:22 +0100

    Remove scheduling setting

--- sunday_school_weekly.py
+++ sunday_school_weekly.py
@@ -1,13 +1,8 @@
 from twitter_bot import tweet_lesson
-import schedule
-import time
 
 
 def main():
-    schedule.every().day.at("14:40").do(tweet_lesson)
-    while True:
-        schedule.run_pending()
-        time.sleep(60)
+    tweet_lesson()
 
 
 main()
